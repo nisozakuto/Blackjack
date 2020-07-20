5a9fea056916e4b4d78371c55387865485479fc4
nisozakuto <nisozakuto@gmail.com>
2020-07-20 14:52:58 -0400

    Bet and money texts

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 console.log("start");
+
+//Variables
+let yourBet = 0;
+let yourMoney = 360;
+
 ///Testing below
 for (let i = 0; i < 100; i++) {
   // let numbers = Math.floor(Math.random() * 12) + 1;
@@ -10,6 +15,11 @@ function test() {
 }
 ////TEST FINISHED
 
+const yourBetText = document.querySelector(".yourBet");
+const yourMoneyText = document.querySelector(".yourMoney");
+yourMoneyText.innerHTML = yourMoney;
+
+
 let shape = Math.floor(Math.random() * 4) + 1;
 let numbers = Math.floor(Math.random() * 12) + 1;
 let playercard1 = document.querySelector(".playerCard");
@@ -20,11 +30,15 @@ const chips = document.querySelectorAll('.chips');
 
 function bet(amount) {
   // console.log("amount", amount.target.id)
+  yourBet += parseInt(amount.target.id);
+  yourMoney -= yourBet;
+  yourBetText.innerHTML = yourBet;
+  yourMoneyText.innerHTML = yourMoney;
 }
 
 chips.forEach((chip, i) => {
   // chips[i].addEventListener('click', bet(chips[i].id));
   chips[i].addEventListener('click', bet);
-  console.log(chip.id);
-  console.log(chip.id);
+  // console.log(chip.id);
+  // console.log(chip.id);
 })
